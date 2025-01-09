a44c0e598b2a40d3e38293911011152f2c817bf9
R0b0so <ethansagar222@gmail.com>
2025-01-08 18:45:22 -0800

    hello

---
+++ intersection.py
@@ -0,0 +1,5 @@
+setx = {"three", "four", "five"}
+sety = {"seven", "six", "five", "four"}
+print("Original set elements:", "\n", setx, "\n", sety)
+setz = setx.intersection(sety)
+print("New set elements", "\n", setz)
