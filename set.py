c1ba0049e878c1de297722df25d4847cc111fdfd
R0b0so <ethansagar222@gmail.com>
2025-01-08 18:36:31 -0800

    hello

---
+++ set.py
@@ -0,0 +1,12 @@
+setx = {1, 3, 2}
+print(setx)
+setx = {"hello", 1, (2, 6, 1), "bye"}
+print(setx)
+setx = {1,2,3,4,2,3,4,5,9}
+print(setx)
+setx = {1,2,3,4,5,6}
+print(setx,"\n")
+setx = {set[1,3,2,6,3,7],3}
+print("Original Set:", "\n", setx)
+setx.pop()
+print("New set:", "\n", setx)
