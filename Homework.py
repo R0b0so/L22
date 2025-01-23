b97df7978d17c1a08584ab01dac27530ac63cde9
R0b0so <ethansagar222@gmail.com>
2025-01-22 18:06:24 -0800

    hello

---
+++ Homework.py
@@ -0,0 +1,5 @@
+setx = {"green", "blue", "yellow"}
+sety = {"red", "green", "orange", "blue"}
+print("Original set elements:", "\n", setx, "\n", sety)
+setz = setx.intersection(sety)
+print("New set elements", "\n", setz)
